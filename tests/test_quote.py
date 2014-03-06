e15a7a8c507588bb48629c1f56058ea4e2ec38c3
Grisha K <naspeh@gmail.com>
2014-03-06 20:52:51 +0200

    Add test for "hide_quote"

---
+++ tests/test_quote.py
@@ -0,0 +1,25 @@
+from lxml import etree
+from pytest import mark
+
+from . import open_file
+from mailr.parser import hide_quote
+
+
+@mark.parametrize('filename', [
+    '1457489417718057053.html', '1456781505677497494.html'
+])
+def test_thread_with_quotes(filename):
+    with open_file('files_quote', filename) as f:
+        thread = etree.fromstring(f.read().decode())
+
+    mails = []
+    for mail in thread.xpath('mail'):
+        subj_ = mail.xpath('subject')[0].text
+        text_ = mail.xpath('text')[0].text
+        html_ = mail.xpath('html')[0].text
+        mails.append({'subj': subj_, 'html': html_, 'text': text_})
+
+    class_ = 'email_quote'
+    for i in range(1, len(mails)):
+        res = hide_quote(mails[i]['html'], mails[i - 1]['html'], class_)
+        assert 'class="email_quote"' in res
