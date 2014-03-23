a337cd88bed245c28b097510c9e2944f73318376
Grisha K <naspeh@gmail.com>
2014-03-23 23:06:04 +0200

    Fix select on ipad

--- mailr/theme/app.js
+++ mailr/theme/app.js
@@ -68,8 +68,7 @@ $('.panel').on('panel_get', function(event, data) {
         // Set content
         panel.html(content);
 
-        panel.find('.labels option').attr('selected', false);
-        panel.find('.labels [value="' + url + '"]').attr('selected', true);
+        panel.find('.labels [value="' + url + '"]').prop('selected', true);
         panel.find('.panel-head')
             .find('.labels').on('change', function() {
                 panel.trigger('panel_get', {url: $(this).find(':selected').val()});
