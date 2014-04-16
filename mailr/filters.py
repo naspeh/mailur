1118afca27030791a641f447ec4f4a4951cd5531
Grisha K <naspeh@gmail.com>
2014-04-16 15:49:32 +0300

    Fix tx_offset

--- mailr/filters.py
+++ mailr/filters.py
@@ -34,7 +34,7 @@ def get_gravatar(addr, size=16, default='identicon'):
 
 @contextfilter
 def localize_dt(ctx, value):
-    tz_offset = ctx.get('env').session['tz_offset']
+    tz_offset = ctx.get('env').session.get('tz_offset')
     return value + dt.timedelta(hours=-(tz_offset or 0))
 
 
