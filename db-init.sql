6df466d867e5cd17bf174a80c5850b204ee8f52e
Grisha K <naspeh@gmail.com>
2014-05-13 01:02:58 +0300

    Update search index

--- db-init.sql
+++ db-init.sql
@@ -4,10 +4,10 @@ CREATE MATERIALIZED VIEW emails_search AS
 SELECT id, gm_thrid,
     setweight(to_tsvector('simple', subject), 'A') ||
     setweight(to_tsvector('simple', text), 'C') ||
-    setweight(to_tsvector(coalesce(array_to_string("from", ','), '')), 'C') ||
-    setweight(to_tsvector(coalesce(array_to_string("to", ','), '')), 'C') ||
-    setweight(to_tsvector(coalesce(array_to_string("cc", ','), '')), 'C') ||
-    setweight(to_tsvector(coalesce(array_to_string("bcc", ','), '')), 'C')
+    setweight(to_tsvector('simple', coalesce(array_to_string("from", ','), '')), 'C') ||
+    setweight(to_tsvector('simple', coalesce(array_to_string("to", ','), '')), 'C') ||
+    setweight(to_tsvector('simple', coalesce(array_to_string("cc", ','), '')), 'C') ||
+    setweight(to_tsvector('simple', coalesce(array_to_string("bcc", ','), '')), 'C')
     as document
 FROM emails;
 
