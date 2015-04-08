67cb0ad4ca9239bd17b0cafa97d3e8c98feb7486
Grisha K <naspeh@gmail.com>
2015-04-08 17:48:18 +0300

    Update sync_gmail

--- mailur/syncer.py
+++ mailur/syncer.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 from uuid import uuid4
 
 from . import imap, parser, log
@@ -11,39 +12,35 @@ def sync_gmail(cur, with_bodies=False):
         if not {'\\All', '\\Junk', '\\Trash'} & set(attrs):
             continue
 
-        fetch_emails(im, name, with_bodies)
+        uids = imap.search(im, name)
+        log.info('"%s" has %i messages' % (name, len(uids)))
+        data = imap.fetch_all(im, uids, 'X-GM-MSGID')
+        uids = OrderedDict((k, v['X-GM-MSGID']) for k, v in data.items())
+
+        fetch_headers(im, uids)
 
 
 @connect()
-def fetch_emails(cur, im, label, with_bodies=False):
-    cur.execute("SELECT extra#>'{X-GM-MSGID}' FROM emails")
-    db_uids = [r[0] for r in cur.fetchall()]
+def fetch_headers(cur, im, map_uids):
+    cur.execute("SELECT extra#>'{X-GM-MSGID}' msgid FROM emails")
+    msgids = [r[0] for r in cur.fetchall()]
 
-    uids = imap.search(im, label)
-    log.info('"%s" has %i messages' % (label, len(uids)))
-    for data in imap.fetch(im, uids, 'X-GM-MSGID'):
-        for uid, row in data.items():
-            if row['X-GM-MSGID'] in db_uids:
-                uids.remove(uid)
-
+    uids = [uid for uid, msgid in map_uids.items() if msgid not in msgids]
     if uids:
-        query = dict([
-            ('time', 'INTERNALDATE'),
-            ('size', 'RFC822.SIZE'),
-            ('header', 'BODY[HEADER]'),
-            ('gm_msgid', 'X-GM-MSGID'),
-        ])
+        query = {
+            'time': 'INTERNALDATE',
+            'size': 'RFC822.SIZE',
+            'header': 'BODY[HEADER]',
+        }
         q = list(query.values())
         for data in imap.fetch(im, uids, q, 'add emails'):
             emails = []
-            for row in data.values():
+            for uid, row in data.items():
                 header = row.pop(query['header'])
-                gm_msgid = row.pop(query['gm_msgid'])
                 fields = {k: row[v] for k, v in query.items() if v in row}
                 fields['id'] = uuid4()
                 fields['thrid'] = fields['id']
-                fields['extra'] = {'X-GM-MSGID': gm_msgid}
-                if not with_bodies:
-                    fields.update(parser.parse(header, fields['id']))
+                fields['extra'] = {'X-GM-MSGID': map_uids[uid]}
+                fields.update(parser.parse(header, fields['id']))
                 emails.append(fields)
             Email.insert(emails)
