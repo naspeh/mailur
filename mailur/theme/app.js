6af6004deb57da61273e7a7177ba2ef49245c4ee
Grisha K <naspeh@gmail.com>
2015-05-29 12:11:49 +0300

    Cleanup js code style

--- mailur/theme/app.js
+++ mailur/theme/app.js
@@ -1,12 +1,15 @@
 var ws = null, handlers = {};
-function guid(){
-    // From http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+
+// Ref: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function guid() {
     var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-        var r = (d + Math.random()*16)%16 | 0;
-        d = Math.floor(d/16);
-        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
-    });
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+        /[xy]/g,
+        function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
     return uuid;
 }
 function connect() {
@@ -15,7 +18,7 @@ function connect() {
         console.log('ws opened');
     };
     ws.onmessage = function(e) {
-        data = JSON.parse(e.data)
+        data = JSON.parse(e.data);
         if (data.uid) {
             console.log('response for ' + data.uid);
             var handler = handlers[data.uid];
@@ -26,9 +29,9 @@ function connect() {
             console.log(data);
             var path = window.location.pathname;
             send(path + '?fmt=body', null, function(data) {
-                if (path.search("^/thread/") != -1) {
+                if (path.search('^/thread/') != -1) {
                     updateEmails(data, true);
-                } else if (path.search("^/in/") != -1) {
+                } else if (path.search('^/in/') != -1) {
                     updateEmails(data);
                 } else {
                     $('body').html(data);
@@ -40,16 +43,18 @@ function connect() {
         console.log('ws closed');
         setTimeout(connect, 10000);
     };
-};
+}
 function send(url, data, callback) {
     if (ws === null) {
         connect();
         send(url, data, callback);
     } else {
         url = 'http://localhost:5000' + url;
-        var resp = {url: url, payload: data, uid: guid()}
+        var resp = {url: url, payload: data, uid: guid()};
         ws.send(JSON.stringify(resp));
-        handlers[resp.uid] = callback;
+        if (callback) {
+            handlers[resp.uid] = callback;
+        }
     }
 }
 function updateEmails(data, thread) {
@@ -92,7 +97,7 @@ $('.email').on('click', '.email-quote-toggle', function() {
 $('.email').on('click', '.email-pin', function() {
     var email = $(this).parents('.email');
     var id = email.data('id');
-    var action = email.hasClass('email-pinned') ? 'rm': 'add';
+    var action = email.hasClass('email-pinned') ? 'rm' : 'add';
     send('/mark/', {ids: [id], name: '\\Starred', action: action});
     return false;
-})
+});
