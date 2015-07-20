fcac94ea4495c2b3ad0e7936b56ce2f149e667b3
Grisha K <naspeh@gmail.com>
2015-07-20 21:11:52 +0300

    Reorganize js code

--- mailur/theme/app.js
+++ mailur/theme/app.js
@@ -1,87 +1,8 @@
+(function() {
 var ws = null, handlers = {}, messages = [];
 
-// Ref: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
-function guid() {
-    var d = new Date().getTime();
-    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
-        /[xy]/g,
-        function(c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d / 16);
-            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
-        });
-    return uuid;
-}
-function connect() {
-    ws = new WebSocket('ws://localhost:9000');
-    ws.onopen = function() {
-        console.log('ws opened');
-        while (messages.length > 0) {
-            messages.pop()();
-        }
-    };
-    ws.onerror = function(error) {
-        console.log('ws error', error);
-    };
-    ws.onmessage = function(e) {
-        data = JSON.parse(e.data);
-        if (data.uid) {
-            console.log('response for ' + data.uid);
-            var handler = handlers[data.uid];
-            if (handler) {
-                handler(data.payload);
-                delete handlers[data.uid];
-            }
-        } else if (data.updated) {
-            console.log(data);
-            var path = location.pathname + location.search;
-            send(path, null, function(data) {
-                if (path.search('^/thread/') != -1) {
-                    updateEmails(data, true);
-                } else if (path.search('^/emails/') != -1) {
-                    updateEmails(data);
-                } else {
-                    $('body').html(data);
-                }
-            });
-        }
-    };
-    ws.onclose = function(event) {
-        ws = null;
-        console.log('ws closed', event);
-        setTimeout(connect, 10000);
-    };
-}
-function send(url, data, callback) {
-    if (ws && ws.readyState === ws.OPEN) {
-        url = 'http://localhost' + url;
-        var resp = {url: url, payload: data, uid: guid()};
-        ws.send(JSON.stringify(resp));
-        if (callback) {
-            handlers[resp.uid] = callback;
-        }
-    } else {
-        messages = [function() {send(url, data, callback);}].concat(messages);
-    }
-}
-function updateEmails(data, thread) {
-    var container = $('.emails');
-    $(data).find('.email').each(function() {
-        var $this = $(this);
-        var exists = $('#' + $this.attr('id'));
-        if (exists.length && $this.data('hash') != exists.data('hash')) {
-            exists.replaceWith(this);
-        } else if (!exists.length) {
-            if (thread) {
-                container.append(this);
-            } else {
-                container.prepend(this);
-            }
-        }
-    });
-}
-
 connect();
+
 if ($('.thread .email-unread').length !== 0) {
     mark({action: '-', name: '\\Unread'});
 }
@@ -123,139 +44,7 @@ $('.emails-byid').on('click', '.email-text a', function() {
 });
 
 (function() {
-var box = $('.compose-to');
-
-if (box.length === 0) return;
-box.selectize({
-    plugins: ['remove_button', 'restore_on_backspace'],
-    delimiter: ',',
-    persist: true,
-    create: true,
-    hideSelected: true,
-    openOnFocus: false,
-    closeAfterSelect: true,
-    copyClassesToDropdown: true,
-    load: function(q, callback) {
-        if (!q.length) return callback();
-        send('/search-email/?q=' + encodeURIComponent(q), null, function(res) {
-            callback(JSON.parse(res));
-        });
-    }
-});
-
-$('.compose-preview').click(function() {
-    $.post('/preview/', {'body': $('.compose-body').val()}, function(data) {
-        $('.email-html').html(data).show();
-    });
-});
-})();
-
-(function() {
-var box = $('.email-labels-input'),
-    container = box.parents('.email-labels-edit'),
-    url = box.data('baseUrl');
-
-if (box.length === 0) return;
-box.selectize({
-    plugins: ['remove_button'],
-    items: box.data('items'),
-    options: box.data('all'),
-    delimiter: ',',
-    persist: true,
-    valueField: 'name',
-    labelField: 'name',
-    searchField: ['name'],
-    hideSelected: true,
-    openOnFocus: false,
-    closeAfterSelect: true,
-    copyClassesToDropdown: true,
-    render: {
-        item: function(i, e) {
-            return (
-                '<div><a href="' + e(i.url) + '">' + e(i.name) + '</a></div>'
-            );
-        },
-        option: function(i, e) {
-            return '<div>' + e(i.name) + '</div>';
-        }
-    },
-    create: function(input) {
-        return {
-            name: input,
-            url: url + input
-        };
-    }
-});
-
-var getLabels = function() {
-    var labels = [];
-        checked = $('.email-pick input:checked, .thread .email-pick input');
-
-    checked.each(function() {
-        var email = $(this).parents('.email');
-        $.each(email.data('labels') || [], function(index, value) {
-            if ($.inArray(value, labels) == -1) {
-                labels.push(value);
-            }
-        });
-    });
-    container.toggle(checked.length !== 0);
-    return labels;
-};
-var selectize = box[0].selectize;
-selectize.setValue(getLabels());
-
-$('.emails').on('change', '.email-pick', function() {
-    selectize.setValue(getLabels());
-});
-
-var ok = $('.email-labels-ok'),
-    cancel = $('.email-labels-cancel');
-
-ok.on('click', function() {
-    mark({
-        action: '=',
-        name: selectize.getValue().split(','),
-        old_name: getLabels()
-    });
-    return false;
-});
-cancel.on('click', function() {
-    selectize.setValue(getLabels());
-});
-$(container.find('input')).each(function() {
-    Mousetrap(this)
-        .bind(['backspace', 'esc'], function() {
-            selectize.close();
-        })
-        .bind('ctrl+enter', function() {
-            ok.focus().click();
-        });
-});
-})();
-function mark(params) {
-    if ($('.emails').hasClass('thread')) {
-        params.ids = [$('.email').first().data('thrid')];
-        params.thread = true;
-    } else {
-        var field = $('.emails-byid').length > 0 ? 'id' : 'thrid';
-        params.thread = field == 'thrid';
-        params.ids = (
-            $('.email .email-pick input:checked')
-            .map(function() {
-                return $(this).parents('.email').data(field);
-            })
-            .get()
-        );
-    }
-    send('/mark/', params);
-}
-function goToLabel(label) {
-    return function() {
-        location.href = '/emails/?in=' + label;
-    };
-}
-
+/* Keyboard shortcuts */
 var hotkeys = [
     [['* a'], 'Select all conversations', function() {
         $('.email .email-pick input').prop('checked', true);
@@ -345,3 +134,225 @@ var hotkeys = [
 $(hotkeys).each(function(index, item) {
     Mousetrap.bind(item[0], item[2]);
 });
+function goToLabel(label) {
+    return function() {
+        location.href = '/emails/?in=' + label;
+    };
+}
+})();
+
+(function() {
+/* Compose */
+var box = $('.compose-to');
+
+if (box.length === 0) return;
+box.selectize({
+    plugins: ['remove_button', 'restore_on_backspace'],
+    delimiter: ',',
+    persist: true,
+    create: true,
+    hideSelected: true,
+    openOnFocus: false,
+    closeAfterSelect: true,
+    copyClassesToDropdown: true,
+    load: function(q, callback) {
+        if (!q.length) return callback();
+        send('/search-email/?q=' + encodeURIComponent(q), null, function(res) {
+            callback(JSON.parse(res));
+        });
+    }
+});
+
+$('.compose-preview').click(function() {
+    $.post('/preview/', {'body': $('.compose-body').val()}, function(data) {
+        $('.email-html').html(data).show();
+    });
+});
+})();
+
+(function() {
+/* Edit labels */
+var box = $('.email-labels-input'),
+    container = box.parents('.email-labels-edit'),
+    url = box.data('baseUrl');
+
+if (box.length === 0) return;
+box.selectize({
+    plugins: ['remove_button'],
+    items: box.data('items'),
+    options: box.data('all'),
+    delimiter: ',',
+    persist: true,
+    valueField: 'name',
+    labelField: 'name',
+    searchField: ['name'],
+    hideSelected: true,
+    openOnFocus: false,
+    closeAfterSelect: true,
+    copyClassesToDropdown: true,
+    render: {
+        item: function(i, e) {
+            return (
+                '<div><a href="' + e(i.url) + '">' + e(i.name) + '</a></div>'
+            );
+        },
+        option: function(i, e) {
+            return '<div>' + e(i.name) + '</div>';
+        }
+    },
+    create: function(input) {
+        return {
+            name: input,
+            url: url + input
+        };
+    }
+});
+
+var selectize = box[0].selectize;
+selectize.setValue(getLabels());
+
+$('.emails').on('change', '.email-pick', function() {
+    selectize.setValue(getLabels());
+});
+
+var ok = $('.email-labels-ok'),
+    cancel = $('.email-labels-cancel');
+
+ok.on('click', function() {
+    mark({
+        action: '=',
+        name: selectize.getValue().split(','),
+        old_name: getLabels()
+    });
+    return false;
+});
+cancel.on('click', function() {
+    selectize.setValue(getLabels());
+});
+$(container.find('input')).each(function() {
+    Mousetrap(this)
+        .bind(['backspace', 'esc'], function() {
+            selectize.close();
+        })
+        .bind('ctrl+enter', function() {
+            ok.focus().click();
+        });
+});
+
+function getLabels() {
+    var labels = [];
+        checked = $('.email-pick input:checked, .thread .email-pick input');
+
+    checked.each(function() {
+        var email = $(this).parents('.email');
+        $.each(email.data('labels') || [], function(index, value) {
+            if ($.inArray(value, labels) == -1) {
+                labels.push(value);
+            }
+        });
+    });
+    container.toggle(checked.length !== 0);
+    return labels;
+}
+})();
+
+/* Related functions */
+function connect() {
+    ws = new WebSocket('ws://localhost:9000');
+    ws.onopen = function() {
+        console.log('ws opened');
+        while (messages.length > 0) {
+            messages.pop()();
+        }
+    };
+    ws.onerror = function(error) {
+        console.log('ws error', error);
+    };
+    ws.onmessage = function(e) {
+        data = JSON.parse(e.data);
+        if (data.uid) {
+            console.log('response for ' + data.uid);
+            var handler = handlers[data.uid];
+            if (handler) {
+                handler(data.payload);
+                delete handlers[data.uid];
+            }
+        } else if (data.updated) {
+            console.log(data);
+            var path = location.pathname + location.search;
+            send(path, null, function(data) {
+                if (path.search('^/thread/') != -1) {
+                    updateEmails(data, true);
+                } else if (path.search('^/emails/') != -1) {
+                    updateEmails(data);
+                } else {
+                    $('body').html(data);
+                }
+            });
+        }
+    };
+    ws.onclose = function(event) {
+        ws = null;
+        console.log('ws closed', event);
+        setTimeout(connect, 10000);
+    };
+}
+// Ref: http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+function guid() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(
+        /[xy]/g,
+        function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+        });
+    return uuid;
+}
+function send(url, data, callback) {
+    if (ws && ws.readyState === ws.OPEN) {
+        url = 'http://localhost' + url;
+        var resp = {url: url, payload: data, uid: guid()};
+        ws.send(JSON.stringify(resp));
+        if (callback) {
+            handlers[resp.uid] = callback;
+        }
+    } else {
+        messages = [function() {send(url, data, callback);}].concat(messages);
+    }
+}
+function updateEmails(data, thread) {
+    var container = $('.emails');
+    $(data).find('.email').each(function() {
+        var $this = $(this);
+        var exists = $('#' + $this.attr('id'));
+        if (exists.length && $this.data('hash') != exists.data('hash')) {
+            exists.replaceWith(this);
+        } else if (!exists.length) {
+            if (thread) {
+                container.append(this);
+            } else {
+                container.prepend(this);
+            }
+        }
+    });
+}
+function mark(params) {
+    if ($('.emails').hasClass('thread')) {
+        params.ids = [$('.email').first().data('thrid')];
+        params.thread = true;
+    } else {
+        var field = $('.emails-byid').length > 0 ? 'id' : 'thrid';
+        params.thread = field == 'thrid';
+        params.ids = (
+            $('.email .email-pick input:checked')
+            .map(function() {
+                return $(this).parents('.email').data(field);
+            })
+            .get()
+        );
+    }
+    send('/mark/', params);
+}
+
+})();
