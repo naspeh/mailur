e16ff9add56d8e50f1a64aa5ba3614aad430c072
Grisha K <naspeh@gmail.com>
2015-08-08 15:50:33 +0300

    Update email preview line

--- mailur/filters.py
+++ mailur/filters.py
@@ -47,6 +47,7 @@ def format_dt(env, value, fmt='%a, %d %b, %Y at %H:%M'):
 def get_preview(text, files):
     files = ', '.join(v['name'] for v in files.values())
     files = ('[%s]' % files) if files else ''
+    text = re.sub(r'\s+', ' ', text)
     text = ' '.join([text or '', files])
     return text[:200].strip() or '>'
 
