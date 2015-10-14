6c368ae7e18152b65716e7820ebd6e30066503ba
Grisha K <naspeh@gmail.com>
2015-10-14 10:57:20 +0300

    Add polyfill for `string.startsWith`

--- front/polyfills.js
+++ front/polyfills.js
@@ -5,3 +5,5 @@ require('core-js/fn/symbol');
 require('core-js/fn/array/of');
 require('core-js/fn/array/from');
 require('core-js/fn/object/assign');
+require('core-js/fn/string/starts-with');
+require('core-js/fn/string/ends-with');
