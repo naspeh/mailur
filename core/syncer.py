eca7d875bd2f78fc54d4909eabb3755a42bad9a4
Grisha K <naspeh@gmail.com>
2015-10-23 15:13:27 +0300

    Fix `update_thrids`

--- core/syncer.py
+++ core/syncer.py
@@ -579,7 +579,7 @@ def update_thrids(env, folder=None, manual=True, commit=True):
                 thrid = parent[0]['thrid']
                 parent = parent[0]['id']
 
-        if thrid is None and row['fr']:
+        if thrid is None and row['fr'] and row['to']:
             parent = env.sql('''
             SELECT id, thrid FROM emails
             WHERE
