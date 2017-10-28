97f15e090dd9ebfbf3f23ac606defe3837d759c3
Grisha K <naspeh@gmail.com>
2017-10-28 22:17:50 +0300

    Trying to load emails through websocket

--- mailur/app.py
+++ mailur/app.py
@@ -1,11 +1,13 @@
+import functools as ft
 import json
 import pathlib
 import re
 
+from gevent.lock import RLock
 from geventwebsocket import WebSocketError
 from webob import Response, dec, exc
 
-from . import log, local
+from . import log, local, imap
 
 static = pathlib.Path(__file__).parent / 'static'
 routes = re.compile('^/(%s)$' % '|'.join((
@@ -123,9 +125,116 @@ def msg(uid, box=local.SRC):
     return Response(txt, content_type='text/plain')
 
 
+ws_handlers = {}
+
+
+def ws_send(ws, lock, uid, body):
+    msg = json.dumps({'uid': uid, 'body': body})
+    log.debug(msg)
+    with lock:
+        return ws.send(msg)
+
+
+def ws_handler(fn):
+    ws_handlers[fn.__name__] = fn
+    return fn
+
+
 def websocket(ws):
+    lock = RLock()
     try:
-        message = ws.receive()
-        ws.send(message)
+        while True:
+            msg = ws.receive()
+            log.debug(msg)
+            if msg is None:
+                break
+
+            msg = json.loads(msg)
+            target = msg['target']
+            handler = ws_handlers['ws_%s' % target]
+            send = ft.partial(ws_send, ws, lock, msg['uid'])
+            handler(send, **msg['params'])
     except WebSocketError as e:
         log.exception(e)
+
+
+@ws_handler
+def ws_ping(send):
+    return send('pong')
+
+
+@ws_handler
+def ws_threads(send, q):
+    con = local.client()
+    thrs = con.thread('REFS UTF-8 INTHREAD REFS %s' % q)
+    log.debug('query: %r; threads: %s', q, len(thrs))
+    if not thrs:
+        send([])
+        return
+    thrs = sum(thrs, [])
+    res = con.sort('(REVERSE DATE)', 'KEYWORD #latest')
+    latest = res[0].decode().split()
+    log.debug('latest %s', len(latest))
+    thrs = [uid for uid in latest if uid in thrs]
+    send(thrs)
+
+    def inner(uids):
+        con = local.client()
+        thrs = con.thread('REFS UTF-8 INTHREAD REFS UID %s' % uids.str)
+
+        all_flags = {}
+        res = con.fetch(sum(thrs, []), 'FLAGS')
+        for line in res:
+            uid, flags = re.search(
+                r'UID (\d+) FLAGS \(([^)]*)\)', line.decode()
+            ).groups()
+            all_flags[uid] = flags
+
+        msgs = {}
+        for thr in thrs:
+            thrid = None
+            thr_flags = []
+            for uid in thr:
+                msg_flags = all_flags[uid]
+                if not msg_flags:
+                    continue
+                thr_flags.append(msg_flags)
+                if '#latest' in msg_flags:
+                    thrid = uid
+            if thrid is None:
+                continue
+            msgs[thrid] = {'flags': list(set(' '.join(thr_flags).split()))}
+
+        log.debug('%s threads', len(msgs))
+        res = con.fetch(msgs, '(BINARY.PEEK[2])')
+        for i in range(0, len(res), 2):
+            uid = res[i][0].decode().split()[2]
+            data = json.loads(res[i][1].decode())
+            msgs[uid].update(data)
+        return send({'msgs': msgs})
+
+    uids = imap.Uids(thrs[:5000], size=1000)
+    uids.call_async(inner, uids)
+
+
+@ws_handler
+def ws_emails(send, q):
+    con = local.client()
+    res = con.sort('(REVERSE DATE)', q)
+    uids = res[0].decode().split()
+    log.debug('query: %r; messages: %s', q, len(uids))
+    if not uids:
+        return []
+    send(uids)
+
+    res = con.fetch(uids[:5000], '(UID FLAGS BINARY.PEEK[2])')
+    msgs = {}
+    for i in range(0, len(res), 2):
+        uid, flags = (
+            re.search(r'UID (\d+) FLAGS \(([^)]*)\)', res[i][0].decode())
+            .groups()
+        )
+        data = json.loads(res[i][1].decode())
+        msgs[uid] = data
+        msgs[uid]['flags'] = flags
+    send({'msgs': msgs})
