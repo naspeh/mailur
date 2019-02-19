d52b05ad69b0f470fc81def1c05b73110fefab49
Grisha K <naspeh@gmail.com>
2019-02-19 22:10:43 +0200

    Expand height of editor if there is more free space

--- mailur/web.py
+++ mailur/web.py
@@ -526,7 +526,7 @@ tpl = '''
 <head>
   <meta
     name="viewport"
-    content="width=device-width; initial-scale=1.0; maximum-scale=1.0;"
+    content="width=device-width, initial-scale=1.0, maximum-scale=1.0"
   />
   <meta charset="utf-8" />
   <title>Mailur: {{title}}</title>
