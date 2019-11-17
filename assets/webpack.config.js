9cedb5317a6bee39333cc274bb0b517e42e1a6ca
Grisha K <naspeh@gmail.com>
2019-11-17 16:17:34 +0200

    Update some node packages

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
-          presets: ['babel-preset-env']
+          presets: ['@babel/preset-env']
         }
       },
       {
